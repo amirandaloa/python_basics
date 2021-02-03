fc3391b608e56d4aaba9521f4936a1ad757faad7
Alejandro <alejandro210417@gmail.com>
2021-02-03 16:56:52 -0500

    Se agrega el cliente tcp para comunicacion con el servidor

---
+++ tcp_client.py
@@ -0,0 +1,22 @@
+import socket
+import logging
+import sys
+
+
+tcp_ip = '127.0.0.1'
+tcp_port = 55000
+buffer_size = 2048
+
+
+try:
+    tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_client.connect((tcp_ip, tcp_port))
+except socket.error as e:
+    logging.error(e.strerror)
+
+output_message = ''
+
+while output_message != 'salir':
+    tcp_client.sendall(output_message.encode('utf-8'))
+
+
